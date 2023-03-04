19197e76361721af0831a9a52157a15434fd8009
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-03-04 22:59:43 +0100

    imported and added global styles

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@ import PageHome from '@/components/PageHome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+    <div class="container">
       <PageHome />
     </div>
   </header>
--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -16,11 +16,37 @@ function getUserById(userId) {
 </script>
 
 <template>
-  <div v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <div v-for="postId in thread.posts" :key="postId">
-      <p>{{ getUserById(getPostById(postId).userId).name }}</p>
-      <p>{{ getPostById(postId).text }}</p>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+
+    <div class="post-list">
+      <div class="post" v-for="postId in thread.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{ getUserById(getPostById(postId).userId).name }}</a>
+
+          <a href="#">
+            <img
+              class="avatar-large"
+              :src="getUserById(getPostById(postId).userId).avatar"
+              alt=""
+            />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{ getPostById(postId).text }}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{ getPostById(postId).publishedAt }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
